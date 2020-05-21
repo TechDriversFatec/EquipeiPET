75d8ac879589a955d5f88804ea78368ea78b3367
Matheus Costa <matheus271200.ms@gmail.com>
2020-05-20 23:11:59 -0300

    Routes, Home e iniciando Navigation

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,12 +2,36 @@ import { createAppContainer } from 'react-navigation';
 
 import { createStackNavigator } from 'react-navigation-stack';
 
+import Home from '../view/HomeScreen';
 import First from '../view/FirstScreen/first';
+import Login from '../view/Login';
+import Registro from '../view/Registro';
 
 const Routes = createAppContainer(
     createStackNavigator({
         First: {
             screen: First,
+            navigationOptions: 
+            {
+                headerShown: false,
+            }
+        },
+        Login: {
+            screen: Login,
+            navigationOptions: 
+            {
+                headerShown: false,
+            }
+        },
+        Registro: {
+            screen: Registro,
+            navigationOptions: 
+            {
+                headerShown: false,
+            }
+        },
+        Home: {
+            screen: Home,
             navigationOptions: 
             {
                 headerShown: false,
