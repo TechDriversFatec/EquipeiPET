9118731dffce5250d7346cfc4f3c8dfcb665ff1d
thaisrabelo <thais.rabelo@fatec.sp.gov.br>
2020-05-20 23:24:26 -0300

    bug fix

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,14 +2,10 @@ import { createAppContainer } from 'react-navigation';
 
 import { createStackNavigator } from 'react-navigation-stack';
 
-<<<<<<< HEAD
-import First from '../view/Login';
-=======
 import Home from '../view/HomeScreen';
 import First from '../view/FirstScreen/first';
 import Login from '../view/Login';
 import Registro from '../view/Registro';
->>>>>>> 75d8ac879589a955d5f88804ea78368ea78b3367
 
 const Routes = createAppContainer(
     createStackNavigator({
