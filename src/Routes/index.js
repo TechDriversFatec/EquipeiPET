a6406e7b404c6c3e6c3389f6c39b570c3c4d5989
Renan <rendoiris@gmail.com>
2020-06-23 21:57:36 -0300

    Tela de escolha do animal a ser cadastrado.

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -10,15 +10,10 @@ import Form from '../view/Form';
 import Profile from '../view/Profile';
 import Vacinas from '../view/Vacinas';
 import CadVacina from '../view/CadastroVacina';
-<<<<<<< HEAD
-import Parasitario from '../view/CadastroParasitario';
-import EsqueceuSenha from '../view/EsqueceuSenha'
-=======
 import Parasitario from '../view/Parasitario';
 import CadParasitario from '../view/CadastroParasitario';
 import Vermifugo from '../view/Vermifugos'
 import CadVermifugo from '../view/CadastroVermifugo';
->>>>>>> 89deeaa74d7f373b9ce359fea2d8790c6a8a15b9
 
 const Routes = createAppContainer(
     createStackNavigator({
@@ -78,18 +73,6 @@ const Routes = createAppContainer(
                 headerShown: true,
             }
         },
-<<<<<<< HEAD
-
-        EsqueceuSenha: {
-            screen: EsqueceuSenha,
-            navigationOptions:
-            {
-                headerShown: true
-            }
-        },
-
-        
-=======
         Parasitario: {
             screen: Parasitario,
             navigationOptions:
@@ -118,7 +101,6 @@ const Routes = createAppContainer(
                 headerShown: true,
             }
         }
->>>>>>> 89deeaa74d7f373b9ce359fea2d8790c6a8a15b9
     })
 );
 
