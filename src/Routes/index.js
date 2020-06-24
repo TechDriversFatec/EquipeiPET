42833bafb8e9b3c6855a2b2c99f13cccb907da19
Matheus Costa <matheus271200.ms@gmail.com>
2020-06-23 23:03:10 -0300

    Novas rotas

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -14,6 +14,12 @@ import Parasitario from '../view/Parasitario';
 import CadParasitario from '../view/CadastroParasitario';
 import Vermifugo from '../view/Vermifugos'
 import CadVermifugo from '../view/CadastroVermifugo';
+import PickAnimal from '../view/EscolhaAnimalCadastro';
+import Forgot from '../view/EsqueceuSenha';
+import Reset from '../view/ResetarSenha';
+import Change from '../view/TrocarSenha';
+import UpdateDog from '../view/EditarPetDog';
+import UpdateCat from '../view/EditarPetCat';
 
 const Routes = createAppContainer(
     createStackNavigator({
@@ -49,6 +55,7 @@ const Routes = createAppContainer(
             screen: Form,
             navigationOptions: 
             {
+                headerTitle: 'Cadastre seu Pet',
                 headerShown: true,
             }
         },
@@ -56,6 +63,7 @@ const Routes = createAppContainer(
             screen: Profile,
             navigationOptions: 
             {
+                headerTitle: 'Perfil do Pet',
                 headerShown: true
             }
         },
@@ -70,6 +78,7 @@ const Routes = createAppContainer(
             screen: CadVacina,
             navigationOptions: 
             {
+                headerTitle: 'Cadastro de vacina',
                 headerShown: true,
             }
         },
@@ -84,6 +93,7 @@ const Routes = createAppContainer(
             screen: CadParasitario,
             navigationOptions:
             {
+                headerTitle: 'Cadastro de parasitário',
                 headerShown: true,
             }
         },
@@ -94,13 +104,54 @@ const Routes = createAppContainer(
                 headerShown: true,
             }
         },
-        CadVermifugo: {
-            screen: CadVermifugo,
+        PickAnimal: {
+            screen: PickAnimal,
             navigationOptions:
             {
+                headerTitle: 'Escolha o tipo',
+                headerShown: true,
+            }
+        },
+        Forgot: {
+            screen: Forgot,
+            navigationOptions:
+            {
+                headerTitle: 'Esqueceu sua senha?',
+                headerShown: true,
+            }
+        },
+        Reset: {
+            screen: Reset,
+            navigationOptions:
+            {
+                headerTitle: 'Cadastre uma nova senha',
+                headerShown: true,
+            }
+        },
+        Change: {
+            screen: Change,
+            navigationOptions:
+            {
+                headerShown: false,
+            }
+        },
+        UpdateDog: {
+            screen: UpdateDog,
+            navigationOptions:
+            {
+                headerTitle: 'Atualizar informações',
+                headerShown: true,
+            }
+        },
+        UpdateCat: {
+            screen: UpdateCat,
+            navigationOptions:
+            {
+                headerTitle: 'Atualizar informações',
                 headerShown: true,
             }
         }
+
     })
 );
 
